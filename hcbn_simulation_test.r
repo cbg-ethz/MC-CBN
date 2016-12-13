8f93623a20da965de185c1836b0165b341e84573
Susana Posada-Cespedes <susana.posada@bsse.ethz.ch>
2016-12-13 09:57:13 +0100

    [FEATURE] h-cbn simulations wip

--- hcbn_simulation_test.r
+++ hcbn_simulation_test.r
@@ -3,20 +3,23 @@ library(doMC)
 registerDoMC(4)
 
 ################## INPUT OPTIONS ##################
-L = 10                              # number of repetitions
-p = seq(4, 12, 2)                   # number of events
+L = 3                               # number of repetitions
+p = c(4, 6) #seq(4, 12, 2)                   # number of events
 lambda_s = 1                        # sampling rate
 N = unlist(lapply(50*p, min, 1000))  # number of observations / genotypes
 eps = 0.05
+mccbn_path = "/Users/susanap/Documents/software/MC-CBN"
 ##################################################
+
+source(file.path(mccbn_path, "hcbn_functions.r"))
 
 # Set seed for reproducibility
 set.seed(47)
 
-obs_log_likelihood = matrix(0, nrow=p, ncol=L)
-relative_abs_error = matrix(0, nrow=p, ncol=L)
+obs_llhood = matrix(0, nrow=length(p), ncol=L)
+relative_abs_error = matrix(0, nrow=length(p), ncol=L)
 for (i in 1:length(p)) {
-  res_list = foreach(j = 1:L, .combine=rbind) %dopar% {
+  res = foreach(j = 1:L, .combine=rbind) %dopar% {
     poset = make_random_poset(p[i])
     lambdas = runif(p[i], 1/3*lambda_s, 3*lambda_s)
     simulated_obs = sample_genotypes(N[i], poset, sampling_param=lambda_s, 
@@ -26,7 +29,8 @@ for (i in 1:length(p)) {
                                 lambda_s, eps, exact=TRUE)
     error = mean(abs(ret$lambdas - lambdas))/mean(lambdas)
     return(c(llhood, error))
-    
   }
+  obs_llhood[i, ] = res[, 1]
+  relative_abs_error[i, ] = res[, 2]
 }
 
