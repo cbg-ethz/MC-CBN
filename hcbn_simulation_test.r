53ecf0ae1a85b0d296c63a22208451d29f2a8cd8
Susana Posada-Cespedes <susana.posada@bsse.ethz.ch>
2016-12-14 10:12:30 +0100

    [FEATURE] h-cbn simulations wip

--- hcbn_simulation_test.r
+++ hcbn_simulation_test.r
@@ -1,13 +1,13 @@
 library(mccbn)
 library(doMC)
-registerDoMC(4)
 
 ################## INPUT OPTIONS ##################
 L = 100                             # number of repetitions
 p = seq(4, 12, 2)                   # number of events
 lambda_s = 1                        # sampling rate
-N = unlist(lapply(50*p, min, 1000))  # number of observations / genotypes
+N = unlist(lapply(50*p, min, 1000)) # number of observations / genotypes
 eps = 0.05
+thrds = 4
 hcbn_path = "/Users/susanap/Documents/software/ct-cbn-0.1.04b/"
 datadir = "/Users/susanap/Documents/hivX/CBN/hcbn_sampling/testdata/"
 mccbn_path = "/Users/susanap/Documents/software/MC-CBN"
@@ -33,13 +33,15 @@ write_poset <- function(p, poset, filename, datadir) {
 
 # Set seed for reproducibility
 set.seed(47)
+registerDoMC(thrds)
+OMPthreads = paste("OMP_NUM_THREADS=", thrds, " ", sep="")
 
-obs_llhood_mc = matrix(0, nrow=length(p), ncol=L)
-relative_abs_error_mc = matrix(0, nrow=length(p), ncol=L)
-runtime_mc = matrix(0, nrow=length(p), ncol=L)
-obs_llhood_mc = matrix(0, nrow=length(p), ncol=L)
-relative_abs_error_mc = matrix(0, nrow=length(p), ncol=L)
-runtime_mc = matrix(0, nrow=length(p), ncol=L)
+obs_llhood_mc           = matrix(0, nrow=length(p), ncol=L)
+relative_abs_error_mc   = matrix(0, nrow=length(p), ncol=L)
+runtime_mc              = matrix(0, nrow=length(p), ncol=L)
+obs_llhood_hcbn         = matrix(0, nrow=length(p), ncol=L)
+relative_abs_error_hcbn = matrix(0, nrow=length(p), ncol=L)
+runtime_hcbn            = matrix(0, nrow=length(p), ncol=L)
 
 for (i in 1:length(p)) {
   res = foreach(j = 1:L, .combine=rbind) %dopar% {
@@ -49,7 +51,7 @@ for (i in 1:length(p)) {
                                      lambdas=lambdas, eps=eps)
     t0 <- Sys.time()
     ret = MCMC_hcbn(poset, simulated_obs$obs_events)
-    run_t_mc = difftime(Sys.time(), t0)
+    run_t_mc = as.numeric(difftime(Sys.time(), t0))
     llhood_mc = obs_log_likelihood(simulated_obs$obs_events, poset, ret$lambdas,
                                 lambda_s, ret$eps, L=10000)
     error_mc = mean(abs(ret$lambdas - lambdas))/mean(lambdas)
@@ -63,27 +65,39 @@ for (i in 1:length(p)) {
     
     write_poset(p[i], poset, filename, datadir)
     
+    if (!dir.exists(file.path(datadir, filename))) {
+      dir.create(file.path(datadir, filename))
+    }
+    
     t0 <- Sys.time()
-    system(paste(hcbn_path, "h-cbn -f", datadir, filename, " -w > ", datadir,
+    system(paste(OMPthreads, hcbn_path, "h-cbn -f", datadir, filename, " -w > ", datadir,
                  filename, ".out.txt", sep=""))
-    run_t_hcbn = difftime(Sys.time(), t0)
+    run_t_hcbn = as.numeric(difftime(Sys.time(), t0))
     lambdas_hcbn = read.csv(file.path(datadir, 
                                       paste(filename, ".lambda", sep="")))
     lambdas_hcbn = as.vector(t(lambdas_hcbn))
-    # TODO: read eps from output file 
-    # Alternative read from output file
-    llhood_hcbn = obs_log_likelihood(simulated_obs$obs_events, poset, 
-                                     lambdas_hcbn, lambda_s, eps_hcbn, L=10000)
-    error_hcbn = mean(abs(lambdas_hcbn - lambdas))/mean(lambdas)
     
+    res_hcbn = read.table(file.path(datadir, paste(filename, ".out.txt", sep="")),
+                          skip=1)
+    llhood_hcbn = as.numeric(res_hcbn[3])
+    error_hcbn = mean(abs(lambdas_hcbn - lambdas))/mean(lambdas)
     
     return(c(llhood_mc, error_mc, run_t_mc, llhood_hcbn, error_hcbn, run_t_hcbn))
   }
   obs_llhood_mc[i, ] = res[, 1]
   relative_abs_error_mc[i, ] = res[, 2]
   runtime_mc[i, ] = res[, 3]
-  obs_llhood_hcbn[i, ] = res[, 1]
-  relative_abs_error_hcbn[i, ] = res[, 2]
-  runtime_hcbn[i, ] = res[, 3]
+  obs_llhood_hcbn[i, ] = res[, 4]
+  relative_abs_error_hcbn[i, ] = res[, 5]
+  runtime_hcbn[i, ] = res[, 6]
 }
 
+save(obs_llhood_mc, file=file.path(datadir,"obs_llhood_mc.Rdata"))
+save(relative_abs_error_mc, 
+     file=file.path(datadir, "relative_abs_error_mc.Rdata"))
+save(runtime_mc, file=file.path(datadir, "runtime_mc.Rdata"))
+
+save(obs_llhood_hcbn, file=file.path(datadir,"obs_llhood_hcbn.Rdata"))
+save(relative_abs_error_hcbn, 
+     file=file.path(datadir, "relative_abs_error_hcbn.Rdata"))
+save(runtime_hcbn, file=file.path(datadir, "runtime_hcbn.Rdata"))
