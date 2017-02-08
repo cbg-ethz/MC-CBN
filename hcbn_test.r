adcd63ef84e925e2237b34159dd1fc236581ee86
Susana Posada-Cespedes <susana.posada@bsse.ethz.ch>
2017-02-08 13:52:11 +0100

    [FEATURE] sampling scheme: perturb compatible genotypes by either adding or removing observations

--- hcbn_test.r
+++ hcbn_test.r
@@ -10,64 +10,15 @@ set.seed(10)
 p = 5                      # number of events
 poset = random_poset(p)    # true poset
 lambda_s = 1               # sampling rate
-N = 1000                   # number of observations / genotypes
+N = 100 #1000                   # number of observations / genotypes
 eps = 0.05
 hcbn_path = "/Users/susanap/Documents/software/ct-cbn-0.1.04b/"
 datadir = "/Users/susanap/Documents/hivX/CBN/hcbn_sampling/testdata/"
+outdir = "/Users/susanap/Documents/hivX/CBN/hcbn_sampling/output/" 
 mccbn_path = "/Users/susanap/Documents/software/MC-CBN"
 ##################################################
 
 source(file.path(mccbn_path, "hcbn_functions.r"))
-
-empirical_vs_sampling <- function(events, L, rep=NULL, one_genotype=FALSE, 
-                                  outname="") {
-  
-  if (one_genotype) {
-    genotype = events
-    N = rep
-  } else {
-    N = nrow(events)
-  }
-  
-  prob_empirical = numeric(N)
-  prob_sampling  = numeric(N)
-  
-  for (i in 1:N) {
-    if (!one_genotype) {
-      genotype = simulated_obs$obs_events[i, ]
-    }
-    prob_empirical[i] = geno_prob_empirical(N=100000, poset, lambdas, lambda_s, 
-                                            genotype, eps=eps)
-    prob_sampling[i] = prob_importance_sampling(genotype, L=L, poset, lambdas, 
-                                lambda_s, eps=eps)
-  }
-  
-  outname = file.path("~/Documents/hivX/CBN/hcbn_sampling/output/", 
-                      paste("L", L, sep=""), 
-                      paste("probability_Y_empirical_vs_sampling" , outname, 
-                            ".pdf", sep=""))
-  
-  if (one_genotype) {
-    df = data.frame(x = c(prob_empirical, prob_sampling), 
-                    type = c(rep("empirical", N), rep("sampling", N)))
-    pl = ggplot(df, aes(x = x, fill = type))
-    pl = pl + geom_histogram(binwidth=0.01, alpha=0.5, position="identity") + 
-      labs(x=expression(P(Y)), y="Frequency") + 
-      theme_bw() + theme(text=element_text(size=14))
-    
-  } else {
-    df = data.frame(x = prob_empirical, y = prob_sampling)
-    pl = ggplot(df, aes(x = x, y = y))
-    pl = pl + geom_point() + 
-      geom_abline(intercept = 0, slope = 1, colour="blue") + 
-      xlab(expression(P[empirical](Y))) + ylab(expression(widehat(P)(Y)~"\n")) +
-      theme_bw() + theme(text=element_text(size=14))
-    
-  }
-  ggsave(outname, pl, width=5, height=3)
-  
-  return(list("empirical" = prob_empirical, "sampling" = prob_sampling))
-}
 
 ###################################################################################
 ### MAIN PROGRAM
@@ -82,54 +33,42 @@ simulated_obs = sample_genotypes(N, poset, sampling_param=lambda_s, lambdas=lamb
                                  eps=eps)
 
 plot_poset(poset)
-# NOTE: For p = 5, there are 32 possible genotypes. However, 16 are compatible with
-#       poset
+# NOTE: For p = 5, there are 32 possible genotypes. However, 16 are compatible 
+#       with poset
 
-###################################################################################
+###############################################################################
 ### TEST 1
-###################################################################################
-# Check how well the sampling scheme approximates P(Y), Expected time differences and
-# expected distance
+###############################################################################
+# Check how well the sampling scheme approximates P(Y), Expected time differences 
+# and expected distance
 ###################### P(Y) ######################
 L = 1000
-probs = empirical_vs_sampling(simulated_obs$obs_events, L=L, 
-                              outname=paste("_L", L, sep=""))
+# binwidth: L=10 0.1; 
+probs = empirical_vs_sampling(simulated_obs$obs_events, L=L, sampling='naive', 
+                              outdir=outdir, outname=paste("_N", N, "_L", L, sep=""))
 
 # One genotype
 genotype = simulated_obs$obs_events[1, ] 
-probs = empirical_vs_sampling(genotype, L=L, rep=N, one_genotype=TRUE, 
-                              outname=paste("_g1_L", L, sep=""))
+probs = empirical_vs_sampling(genotype, L=L, rep=N, one_genotype=TRUE,
+                              sampling='naive', outdir=outdir, 
+                              outname=paste("_g1_rep", N, "_L", L, sep=""), 
+                              binwidth=0.01)
 
 # WT
 genotype = rep(0, p)
 probs = empirical_vs_sampling(genotype, L=L, rep=N, one_genotype=TRUE, 
-                              outname=paste("_WT_L", L, sep=""))
+                              sampling='naive', outdir=outdir,
+                              outname=paste("_WT_rep", N, "_L", L, sep=""),
+                              binwidth=0.01)
 
 # Resistant type
 genotype = rep(1, p)
 probs = empirical_vs_sampling(genotype, L=L, rep=N, one_genotype=TRUE, 
-                              outname=paste("_RT_L", L, sep=""))
-# P(Y)
-prob_empirical = numeric(N)
-prob_sampling  = numeric(N)
-for (i in 1:N) {
-  genotype = simulated_obs$obs_events[i, ]
-  prob_empirical[i] = geno_prob_empirical(N=100000, poset, lambdas, lambda_s, genotype, 
-                                          eps=eps)
-  prob_sampling[i] = prob_importance_sampling(genotype, L=100, poset, lambdas, lambda_s, eps=eps)
-}
+                              sampling='naive', outdir=outdir,
+                              outname=paste("_RT_rep", N, "_L", L, sep=""),
+                              binwidth=0.01)
 
-df = data.frame(x = prob_empirical, y = prob_sampling)
-pl = ggplot(df, aes(x = x, y = y))
-pl = pl + geom_point() + 
-  geom_abline(intercept = 0, slope = 1, colour="blue") + 
-  labs(x = "\nEmpirical probability", y = "Estimated probability\n" ) +
-  theme_bw() +
-  theme(text=element_text(size=14))
-
-pl
-
-# Time differences 
+###################### Expected time differences ######################
 time_diff_empirical = matrix(0, ncol=p, nrow=N)
 time_diff_sampling  = matrix(0, ncol=p, nrow=N)
 for (i in 1:N) {
@@ -174,7 +113,9 @@ pl = pl + geom_point() +
 
 pl
 
-################ TEST2 ################
+###############################################################################
+### TEST 2
+###############################################################################
 set.seed(10)
 dist = rowSums(simulated_obs$obs_events != simulated_obs$hidden_genotypes)
 llhood = complete_log_likelihood(lambdas, simulated_obs$T_events, dist, eps)
@@ -214,7 +155,9 @@ abs(lambdas_hcbn - lambdas)/lambdas
 obs_log_likelihood(simulated_obs$obs_events, poset, lambdas_hcbn, lambda_s, 
                    0.034023, L=10000) # N=100, -268.256
 
-################ TEST3 ################
+###############################################################################
+### TEST 3
+###############################################################################
 set.seed(10)
 # empty poset
 poset = matrix(0, p, p)
